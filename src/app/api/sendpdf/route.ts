bbefddb58eb56a184bcf9946b44e240851f731d0
Kbueno1077 <kbueno1077@gmail.com>
2024-03-19 23:10:16 -0400

    remove limit

--- src/app/api/sendpdf/route.ts
+++ src/app/api/sendpdf/route.ts
@@ -7,7 +7,6 @@ import { EmploymentTemplate } from "~/components/EmailTemplates/ContactUsTemplat
 
 const resend = new Resend("re_bk97hei7_CEzFhTfxUCbUcMbBB1fRYEc1");
 
-export const limit = "20mb";
 export const maxDuration = 20;
 
 export async function POST(req: NextRequest, res: NextResponse) {
