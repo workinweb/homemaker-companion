49bfd2cf663c677174beaf883668628aad9c3d76
Kbueno1077 <kbueno1077@gmail.com>
2024-04-04 16:47:09 -0400

    Floating Button Styles

--- src/modules/FloatingButton/FloatingButton.tsx
+++ src/modules/FloatingButton/FloatingButton.tsx
@@ -1,21 +1,29 @@
-'use client'
+"use client";
 
 import React from "react";
-import {Button} from "@nextui-org/react";
-import {FaArrowUp} from "react-icons/fa6";
+import { Button } from "@nextui-org/react";
+import { FaArrowUp } from "react-icons/fa6";
 
 export function FloatingButton() {
-
-
     const goToTop = () => {
-        window.scroll({top: 0, left: 0, behavior: 'smooth'})
-    }
+        window.scroll({ top: 0, left: 0, behavior: "smooth" });
+    };
 
     return (
-        <div id="floating-btn" className='lg:hidden fixed bottom-5 right-5'>
-            <Button onPress={goToTop} isIconOnly color="primary" aria-label="Go To Top Button">
-                <FaArrowUp/>
+        <div
+            id="floating-btn"
+            className="fixed bottom-5 right-5 z-[100] lg:hidden"
+        >
+            <Button
+                onPress={goToTop}
+                className="border-gray-300 bg-primary text-white"
+                isIconOnly
+                variant="bordered"
+                color="primary"
+                aria-label="Go To Top Button"
+            >
+                <FaArrowUp />
             </Button>
-        </div>);
+        </div>
+    );
 }
-
